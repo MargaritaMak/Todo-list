53be3e748cbe31668c1fe092ea7d6c77b3bb8ee0
MargaritaMak <margarita.pogosyan.96@mail.ru>
2020-06-04 14:54:39 +0400

    'added new functionalities'

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ for(let i = 0; i < close.length; i++){
     close[i].onclick = function(){
         let elem = document.getElementsByClassName('list');
         elem[i].style.display ='none';
-        console.log(elem)
     }
 }
+
+
+function newElement() {
+    var li = document.createElement("li");
+    li.className = 'list';
+    var inputValue = document.getElementById("myInput").value;
+    var text = document.createTextNode(inputValue);
+   li.appendChild(text);
+    if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     document.getElementById("todo-list").appendChild(li);
+   }
+   document.getElementById("myInput").value = "";
+
+   var button = document.createElement("BUTTON");
+   var icon = document.createElement("I");
+   icon.className = 'fas fa-window-close';
+   button.className = "close";
+   button.appendChild(icon);
+   li.appendChild(button);
+
+   const close =  document.getElementsByClassName('close');
+    for(let i = 0; i < close.length; i++){
+        close[i].onclick = function(){
+        let elem = document.getElementsByClassName('list');
+        elem[i].style.display ='none';
+    }
+ }
+}
+
+  
