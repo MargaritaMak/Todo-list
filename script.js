0cb1284e3993319dff3e10f34a9b2aaa1b8ba6b2
MargaritaMak <margarita.pogosyan.96@mail.ru>
2020-06-05 21:08:07 +0400

    added new functionalities

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const close =  document.getElementsByClassName('close');
 
-
+function onDelete(){
 for(let i = 0; i < close.length; i++){
     close[i].onclick = function(){
         let elem = document.getElementsByClassName('list');
         elem[i].style.display ='none';
     }
 }
+}
 
+onDelete()
 
 function newElement() {
     var li = document.createElement("li");
@@ -30,12 +32,7 @@ function newElement() {
    li.appendChild(button);
 
    const close =  document.getElementsByClassName('close');
-    for(let i = 0; i < close.length; i++){
-        close[i].onclick = function(){
-        let elem = document.getElementsByClassName('list');
-        elem[i].style.display ='none';
-    }
- }
+   onDelete();
 }
 
   
